276ae5665a6813d83d15063b29b44384d55c9180
Daniel Galdez <daniel.monteiro@tre-ma.jus.br>
2024-10-29 15:47:02 -0300

    Variable names translated to English

--- sort-buttons/sort-buttons.js
+++ sort-buttons/sort-buttons.js
@@ -1,18 +1,18 @@
-function ativaOrdenacao(tabelaID, formularioID) {
-  let formulario = $("#" + formularioID);
+function triggerSorting(tableID, formID) {
+  let form = $("#" + formID);
 
   // Função para atualizar o conteúdo das tooltips de depuração
   function printLocalStorage() {
     $(".label-default").each(function (index) {
       const icons = ["fa-file-code", "fa-question", "fa-arrows-v", "fa-table"];
-      const keys = ["campoSort", "isNumeric", "dirSort", "idTabela"];
+      const keys = ["fieldName", "isNumeric", "dirSort", "tableID"];
       const labels = [
         `Field ref: ${localStorage.getItem(keys[0])}`,
         `isNumber: ${localStorage.getItem(keys[1])}`,
         `Direction: ${localStorage.getItem(keys[2])}`,
         `Table: ${localStorage.getItem(
           keys[3]
-        )} (submited)<i class="fa fa-arrow-right"></i> ${formularioID}`,
+        )} (submited)<i class="fa fa-arrow-right"></i> ${formID}`,
       ];
       $(this)
         .html(`<i class="fa ${icons[index]}"></i> ${labels[index]}`)
@@ -21,143 +21,136 @@ function ativaOrdenacao(tabelaID, formularioID) {
   }
 
   // Função para criar e inserir o overlay dinamicamente
-  function criarOverlay() {
-    let $tabela = $("#" + tabelaID);
-    if ($(`#${tabelaID}Overlay`).length === 0) {
+  function createOverlay() {
+    let $table = $("#" + tableID);
+    if ($(`#${tableID}Overlay`).length === 0) {
       let overlayHTML = `
-					<div id="${tabelaID}Overlay" class="overlay">
+					<div id="${tableID}Overlay" class="overlay">
 						<div class="panel panel-default">
 							<div class="panel-body">
 								<i class="fa fa-2x fa-pulse fa-spinner"></i>
 							</div>
 						</div>
 					</div>`;
-      $tabela.css("position", "relative").append(overlayHTML);
+      $table.css("position", "relative").append(overlayHTML);
     }
   }
-  criarOverlay();
+  createOverlay();
 
   const fromTo = { desc: null, null: "asc", asc: "desc" };
-  const dirIcone = {
+  const iconDir = {
     desc: ["fa-sort-alpha-desc", "fa-sort-numeric-desc"],
     null: ["fa-bars", "fa-bars"],
     asc: ["fa-sort-alpha-asc", "fa-sort-numeric-asc"],
   };
   const dirTooltip = {
-    desc: "Ordenação Decrescente",
-    null: "Clique Para Ordenar",
-    asc: "Ordenação Crescente",
+    desc: "Sort Descending",
+    null: "Click to Sort",
+    asc: "Sort Ascending",
   };
 
-  function campoOculto(formulario, id, name, valor) {
-    let $input = $("#" + id, formulario);
+  function hiddenField(form, id, name, value) {
+    let $input = $("#" + id, form);
     if ($input.length === 0) {
       $input = $("<input>")
         .attr({ type: "hidden", id: id, name: name })
-        .appendTo(formulario);
+        .appendTo(form);
     }
-    $input.val(valor);
-    return valor;
+    $input.val(value);
+    return value;
   }
 
-  function atualizarIconeOrdenacao($icone, novaDirecao, indexIcone) {
-    const classesDeIcone = `${dirIcone[null][indexIcone]} ${
-      dirIcone["asc"][indexIcone]
-    } ${dirIcone["desc"][indexIcone]}`;
-    $icone
-      .removeClass(classesDeIcone)
-      .addClass(dirIcone[novaDirecao][indexIcone])
-      .data("sortdir", novaDirecao);
+  function iconUpdate($icon, newDir, iconIDX) {
+    const iconClasses = `${iconDir[null][iconIDX]} ${iconDir["asc"][iconIDX]} ${
+      iconDir["desc"][iconIDX]
+    }`;
+    $icon
+      .removeClass(iconClasses)
+      .addClass(iconDir[newDir][iconIDX])
+      .data("sortdir", newDir);
   }
 
-  function processaClique(event) {
+  function clickCallback(event) {
     event.preventDefault();
-    let overlay = $("#" + tabelaID + "Overlay");
+    let overlay = $("#" + tableID + "Overlay");
     overlay.show(); // Mostrar overlay
     const $th = $(this);
-    const $icone = $th.find("i");
-    const direcaoAtual = $icone.data("sortdir");
-    const novaDirecao = fromTo[direcaoAtual];
-    const campoTarget = $th.data("nomecampo");
-    const campoOrigin = localStorage.getItem("campoSort");
+    const $icon = $th.find("i");
+    const currentDir = $icon.data("sortdir");
+    const newDir = fromTo[currentDir];
+    const targetField = $th.data("fieldname");
+    const campoOrigin = localStorage.getItem("fieldName");
     const isNumeric = $th.data("isnumeric") || false;
-    const indexIcone = isNumeric ? 1 : 0;
+    const iconIDX = isNumeric ? 1 : 0;
 
-    atualizarIconeOrdenacao($icone, novaDirecao, indexIcone);
-    $th.attr("data-original-title", dirTooltip[novaDirecao]).tooltip("show");
-    $th.toggleClass("th-selecionado", novaDirecao !== null);
+    iconUpdate($icon, newDir, iconIDX);
+    $th.attr("data-original-title", dirTooltip[newDir]).tooltip("show");
+    $th.toggleClass("th-selecionado", newDir !== null);
 
     if (
-      tabelaID + campoOrigin !==
-      localStorage.getItem("idTabela") + campoTarget
+      tableID + campoOrigin !==
+      localStorage.getItem("tableID") + targetField
     ) {
       const $selecionadoAnterior = $(
         `#${localStorage.getItem(
-          "idTabela"
-        )} th[data-nomecampo="${campoOrigin}"]`
+          "tableID"
+        )} th[data-fieldname="${campoOrigin}"]`
       );
-      atualizarIconeOrdenacao($selecionadoAnterior.find("i"), null, indexIcone);
+      iconUpdate($selecionadoAnterior.find("i"), null, iconIDX);
       $selecionadoAnterior
         .removeClass("th-selecionado")
         .attr("data-original-title", dirTooltip.null);
     }
 
-    localStorage.setItem("idTabela", tabelaID);
+    localStorage.setItem("tableID", tableID);
     localStorage.setItem(
       "dirSort",
-      campoOculto(formulario, "dirSort", "dirSort", novaDirecao)
+      hiddenField(form, "dirSort", "dirSort", newDir)
     );
     localStorage.setItem(
-      "campoSort",
-      campoOculto(
-        formulario,
-        "campoSort",
-        "campoSort",
-        novaDirecao ? campoTarget : null
-      )
+      "fieldName",
+      hiddenField(form, "fieldName", "fieldName", newDir ? targetField : null)
     );
     localStorage.setItem(
       "isNumeric",
-      campoOculto(formulario, "isNumeric", "isNumeric", isNumeric)
+      hiddenField(form, "isNumeric", "isNumeric", isNumeric)
     );
 
-    // formulario.submit(); // Descomentar se necessário
+    // form.submit(); // Descomentar se necessário
     setTimeout(() => {
       overlay.hide(); // Ocultar overlay após a simulação de processamento
     }, 500); // Substitua pelo tempo real de processamento ou callback após conclusão
     printLocalStorage(); // Remover ou comentar para produção
   }
 
-  $(`#${tabelaID} th[data-nomecampo]`).each(function () {
-    if (formulario == null) {
+  $(`#${tableID} th[data-fieldname]`).each(function () {
+    if (form == null) {
       return;
     }
     const $th = $(this);
-    const campoTarget = $th.data("nomecampo");
+    const targetField = $th.data("fieldname");
     const isNumeric = $th.data("isnumeric");
-    const indexIcone = isNumeric ? 1 : 0;
-    const ehSelecionado =
-      tabelaID + campoTarget ===
-      localStorage.getItem("idTabela") + localStorage.getItem("campoSort");
-    const direcaoSort = localStorage.getItem("dirSort");
-    const $icone = $('<i class="fa fa-fw icone-ordenacao"></i>')
-      .data("sortdir", ehSelecionado ? direcaoSort : null)
+    const iconIDX = isNumeric ? 1 : 0;
+    const isSelected =
+      tableID + targetField ===
+      localStorage.getItem("tableID") + localStorage.getItem("fieldName");
+    const sortDir = localStorage.getItem("dirSort");
+    const $icon = $('<i class="fa fa-fw icone-ordenacao"></i>')
+      .data("sortdir", isSelected ? sortDir : null)
       .addClass(
-        ehSelecionado
-          ? dirIcone[direcaoSort][indexIcone]
-          : dirIcone[null][indexIcone]
+        isSelected ? iconDir[sortDir][iconIDX] : iconDir[null][iconIDX]
       );
 
     $th
-      .prepend($icone)
+      .prepend($icon)
       .addClass("th-ordenacao")
-      .toggleClass("th-selecionado", ehSelecionado)
-      .on("click", processaClique)
+      .toggleClass("th-selecionado", isSelected)
+      .on("click", clickCallback)
       .attr({
         "data-toggle": "tooltip",
         "data-placement": "top",
         "data-container": "body",
-        title: dirTooltip[ehSelecionado ? direcaoSort : null],
+        title: dirTooltip[isSelected ? sortDir : null],
       })
       .tooltip();
   });
